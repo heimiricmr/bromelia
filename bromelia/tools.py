b2703510550d1a0624fde6dc2201f17d9e1254d0
henriquemr <heimiricmr@gmail.com>
2023-02-22 19:22:12 -0300

    feat: Add ResponseGenerator class

--- bromelia/tools.py
+++ bromelia/tools.py
@@ -13,7 +13,59 @@ from .avps import *
 from .base import (
     DiameterAVP,
     DiameterAnswer,
+    DiameterRequest
 )
+
+
+class ResponseGenerator:
+    def __init__(self, request: DiameterRequest, proxy_response: type[DiameterAnswer]):
+        self.request = request
+        self.proxy_response = proxy_response
+        self.kwargs = {}
+
+
+    def load_avps(self, **kwargs):
+        self.kwargs = kwargs
+
+
+    def missing_avp(self, msg: str = None, avp: DiameterAVP = None):
+        return create_missing_avp_response(self.proxy_response, msg, avp, **self.kwargs)
+
+
+    def invalid_avp_value(self, msg: str = None, avp: DiameterAVP = None):
+        return create_invalid_avp_value_response(self.proxy_response, msg, avp, **self.kwargs)
+
+
+    def user_unknown(self):
+        return create_user_unknown_response(self.proxy_response, **self.kwargs)
+
+
+    def unknown_serving_node(self):
+        return create_unknown_serving_node_response(self.proxy_response, **self.kwargs)
+
+
+    def rat_not_allowed(self):
+        return create_rat_not_allowed_response(self.proxy_response, **self.kwargs)
+
+
+    def roaming_not_allowed(self, msg: str):
+        return create_roaming_not_allowed_response(self.proxy_response, msg, **self.kwargs)
+
+
+    def realm_not_served(self, msg: str):
+        return create_realm_not_served_response(self.proxy_response, msg, **self.kwargs)
+
+
+    def unknown_eps_subscription(self):
+        return create_unknown_eps_subscription_response(self.proxy_response)
+
+
+    def authentication_data_unavailable(self, msg: str = None, avp: DiameterAVP = None):
+        return create_authentication_data_unavailable_response(self.proxy_response, msg, avp)
+
+
+    def success(self, **kwargs):
+        return create_success_response(self.proxy_response, **self.kwargs, **kwargs)
 
 
 def create_experimental_result_data(code: bytes) -> list:
