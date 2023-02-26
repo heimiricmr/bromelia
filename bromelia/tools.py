b13f44080b15328441b716ba5af0c21a1ae224c7
henriquemr <heimiricmr@gmail.com>
2023-02-26 13:11:51 -0300

    chore: Add type hinting to tools module

--- bromelia/tools.py
+++ bromelia/tools.py
@@ -28,43 +28,43 @@ class ResponseGenerator:
         self.kwargs = kwargs
 
 
-    def missing_avp(self, msg: str = None, avp: DiameterAVP = None):
+    def missing_avp(self, msg: str = None, avp: DiameterAVP = None) -> type[DiameterAnswer]:
         return create_missing_avp_response(self.proxy_response, msg, avp, **self.kwargs)
 
 
-    def invalid_avp_value(self, msg: str = None, avp: DiameterAVP = None):
+    def invalid_avp_value(self, msg: str = None, avp: DiameterAVP = None) -> type[DiameterAnswer]:
         return create_invalid_avp_value_response(self.proxy_response, msg, avp, **self.kwargs)
 
 
-    def user_unknown(self):
+    def user_unknown(self) -> type[DiameterAnswer]:
         return create_user_unknown_response(self.proxy_response, **self.kwargs)
 
 
-    def unknown_serving_node(self):
+    def unknown_serving_node(self) -> type[DiameterAnswer]:
         return create_unknown_serving_node_response(self.proxy_response, **self.kwargs)
 
 
-    def rat_not_allowed(self):
+    def rat_not_allowed(self) -> type[DiameterAnswer]:
         return create_rat_not_allowed_response(self.proxy_response, **self.kwargs)
 
 
-    def roaming_not_allowed(self, msg: str):
+    def roaming_not_allowed(self, msg: str) -> type[DiameterAnswer]:
         return create_roaming_not_allowed_response(self.proxy_response, msg, **self.kwargs)
 
 
-    def realm_not_served(self, msg: str):
+    def realm_not_served(self, msg: str) -> type[DiameterAnswer]:
         return create_realm_not_served_response(self.proxy_response, msg, **self.kwargs)
 
 
-    def unknown_eps_subscription(self):
+    def unknown_eps_subscription(self) -> type[DiameterAnswer]:
         return create_unknown_eps_subscription_response(self.proxy_response)
 
 
-    def authentication_data_unavailable(self, msg: str = None, avp: DiameterAVP = None):
+    def authentication_data_unavailable(self, msg: str = None, avp: DiameterAVP = None) -> type[DiameterAnswer]:
         return create_authentication_data_unavailable_response(self.proxy_response, msg, avp)
 
 
-    def success(self, **kwargs):
+    def success(self, **kwargs) -> type[DiameterAnswer]:
         return create_success_response(self.proxy_response, **self.kwargs, **kwargs)
 
 
@@ -85,7 +85,7 @@ def include_error_bit(func):
 
 
 @include_error_bit
-def create_missing_avp_response(proxy_response: type[DiameterAnswer], msg: str = None, avp: DiameterAVP = None, **kwargs):
+def create_missing_avp_response(proxy_response: type[DiameterAnswer], msg: str = None, avp: DiameterAVP = None, **kwargs) -> type[DiameterAnswer]:
     if avp is not None:
         return proxy_response(result_code=DIAMETER_MISSING_AVP,
                               failed_avp=[avp],
@@ -98,7 +98,7 @@ def create_missing_avp_response(proxy_response: type[DiameterAnswer], msg: str =
 
 
 @include_error_bit
-def create_invalid_avp_value_response(proxy_response: type[DiameterAnswer], msg: str = None, avp: DiameterAVP = None, **kwargs):
+def create_invalid_avp_value_response(proxy_response: type[DiameterAnswer], msg: str = None, avp: DiameterAVP = None, **kwargs) -> type[DiameterAnswer]:
     if avp is not None:
         return proxy_response(result_code=DIAMETER_INVALID_AVP_VALUE,
                               failed_avp=[avp],
@@ -111,13 +111,13 @@ def create_invalid_avp_value_response(proxy_response: type[DiameterAnswer], msg:
 
 
 @include_error_bit
-def create_user_unknown_response(proxy_response: type[DiameterAnswer], **kwargs):
+def create_user_unknown_response(proxy_response: type[DiameterAnswer], **kwargs) -> type[DiameterAnswer]:
     return proxy_response(experimental_result=create_experimental_result_data(DIAMETER_ERROR_USER_UNKNOWN),
                           **kwargs)
 
 
 @include_error_bit
-def create_authentication_data_unavailable_response(proxy_response: type[DiameterAnswer], msg: str = None, avp: DiameterAVP = None, **kwargs):
+def create_authentication_data_unavailable_response(proxy_response: type[DiameterAnswer], msg: str = None, avp: DiameterAVP = None, **kwargs) -> type[DiameterAnswer]:
     if avp is not None:
         return proxy_response(result_code=DIAMETER_AUTHENTICATION_DATA_UNAVAILABLE,
                               failed_avp=[avp],
@@ -130,37 +130,37 @@ def create_authentication_data_unavailable_response(proxy_response: type[Diamete
 
 
 @include_error_bit
-def create_unknown_serving_node_response(proxy_response: type[DiameterAnswer], **kwargs):
+def create_unknown_serving_node_response(proxy_response: type[DiameterAnswer], **kwargs) -> type[DiameterAnswer]:
     return proxy_response(experimental_result=create_experimental_result_data(DIAMETER_ERROR_UNKOWN_SERVING_NODE),
                           **kwargs)
 
 
 @include_error_bit
-def create_rat_not_allowed_response(proxy_response: type[DiameterAnswer], **kwargs):
+def create_rat_not_allowed_response(proxy_response: type[DiameterAnswer], **kwargs) -> type[DiameterAnswer]:
     return proxy_response(experimental_result=create_experimental_result_data(DIAMETER_ERROR_RAT_NOT_ALLOWED),
            **kwargs)
 
 
 @include_error_bit
-def create_roaming_not_allowed_response(proxy_response: type[DiameterAnswer], msg: str, **kwargs):
+def create_roaming_not_allowed_response(proxy_response: type[DiameterAnswer], msg: str, **kwargs) -> type[DiameterAnswer]:
     return proxy_response(experimental_result=create_experimental_result_data(DIAMETER_ERROR_ROAMING_NOT_ALLOWED),
                           error_diagnostic=msg,
                           **kwargs)
 
 
 @include_error_bit
-def create_realm_not_served_response(proxy_response: type[DiameterAnswer], msg: str, **kwargs):
+def create_realm_not_served_response(proxy_response: type[DiameterAnswer], msg: str, **kwargs) -> type[DiameterAnswer]:
     return proxy_response(result_code=DIAMETER_REALM_NOT_SERVED,
                           error_message=ErrorMessageAVP(msg),
                           **kwargs)
 
 
 @include_error_bit
-def create_unknown_eps_subscription_response(proxy_response: type[DiameterAnswer], **kwargs):
+def create_unknown_eps_subscription_response(proxy_response: type[DiameterAnswer], **kwargs) -> type[DiameterAnswer]:
     return proxy_response(experimental_result=create_experimental_result_data(DIAMETER_ERROR_UNKNOWN_EPS_SUBSCRIPTION),
                           **kwargs)
 
 
 @include_error_bit
-def create_success_response(proxy_response: type[DiameterAnswer], **kwargs):
+def create_success_response(proxy_response: type[DiameterAnswer], **kwargs) -> type[DiameterAnswer]:
     return proxy_response(result_code=DIAMETER_SUCCESS, **kwargs)
